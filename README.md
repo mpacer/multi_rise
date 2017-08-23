73fc2271ab060dc40604d444642bcc25f10c11e4
M Pacer <mpacer@berkeley.edu>
2017-08-23 16:31:19 -0400

    move things around to make into package

---
+++ README.md
@@ -0,0 +1,10 @@
+If you want to use this create an index.txt file with a sequence of paths to your files each on a new line in the order you wish them to be included in your final presentation.
+
+Then use with this file in the directory.
+
+```bash
+python -m multi_rise --index=index2.txt --output=output.ipynb
+```
+
+
+This only works on python 3.4+.
---
+++ multi_rise/__init__.py
@@ -0,0 +1,29 @@
+"""This is a quick shortcut to combine Jupyter notebooks into a single rise
+presentation.  It adds a cell in between each notebook to reset the environment
+so that the notebooks don't cause issues for each other."""
+
+import argparse
+from pathlib import Path
+
+from multi_rise import combine_nbs
+
+__version__ = '0.1'
+
+
+def parse_index(fp):
+    if isinstance(fp, Path):
+        with fp.open() as f:
+            nb_files = [fname.rstrip() for fname in f.readlines()
+                        if Path(fname.rstrip()).is_file()]
+    return nb_files
+
+
+def main(argv=None):
+    ap = argparse.ArgumentParser(description='Combine notebooks and make slides')
+    ap.add_argument('--index', nargs='?', type=Path,
+                    default='index.txt')
+    ap.add_argument('--output', nargs='?', type=Path,
+                    default='combined_nb.ipynb')
+    args = ap.parse_args(argv)
+
+    combine_nbs(nb_files=parse_index(args.index), output=args.output)
